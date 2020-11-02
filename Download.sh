ebd01fd88c83e5e9893d5cec8cb6746257f133ad
sempogi <55341620+sempogi@users.noreply.github.com>
2020-11-03 00:03:48 +0800

    Update Download.sh

--- Download.sh
+++ Download.sh
@@ -4,6 +4,7 @@ cd #home directory
 curl https://raw.githubusercontent.com/sempogi/termuxurl/main/installytandspoti.sh -o spoty.sh
 dos2unix spoty.sh
 bash spoty.sh
+
 #1. Install termux from playstore
 #2. Copy paste this script to termux terminal
 #3. Done
