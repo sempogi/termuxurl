ae3ad62c10949ae9f406422015889d044e69222d
sempogi <55341620+sempogi@users.noreply.github.com>
2020-11-02 22:41:24 +0800

    Add files via upload
    Downloader installer

---
+++ installytandspoti.sh
@@ -0,0 +1,44 @@
+
+#!/data/data/com.termux/files/usr/bin/bash
+#credit to raj for yt installer and the i forgot.(Update soon)
+echo "Updating default packages"
+apt update && apt -y upgrade
+# Install packages
+pkg install -y nodejs ffmpeg #mp3 sipport
+
+# Get spotify-dl from npmjs
+echo "[ spotifydl ] Installing spotify-dl"
+npm install -g spotify-dl
+
+# Setup app sharing script
+echo "[ spotifydl ] Setting up scripts..."
+
+if [ ! -d "$HOME/bin" ]; then #just init
+    mkdir "$HOME/bin"
+fi
+#--------------------------------------------------------
+
+echo "Installing Required Tools"
+apt install termux-api figlet python ffmpeg -y
+
+echo "Requesting access to storage"
+termux-setup-storage
+sleep 5
+
+echo "Installing Dependencies"
+yes | pip install youtube-dl
+
+echo "Creating the Youtube folder to download the files"
+mkdir ~/storage/shared/youtube
+
+echo "Creating youtube-dl folder for config"
+mkdir -p ~/.config/youtube-dl #fix 
+
+echo "Creating bin folder"
+mkdir ~/bin
+#------------------------- Installing
+echo "Downloading and installing termux-url-opener"
+curl https://raw.githubusercontent.com/sempogi/termuxurl/main/Termuxurl -o ~/bin/termux-url-opener
+sleep 2
+dos2unix ~/bin/termux-url-opener
+echo " Video Downloader Install Successful"'
