c84712ec57158daf93a61aca4329a3faaccc179d
sem sabiduria <semsabiduria@gmail.comgit config --global user.email semsabiduria@gmail.comgit config --global user.email semsabiduria@gmail.com>
2020-11-07 07:25:37 +0800

    termux

--- upload.sh
+++ upload.sh
@@ -1,8 +1,15 @@
 cd termuxurl
+if ["$1"=="" -o "$2"==""];
+ then
+echo " I need username and Password"
+
+exit 
+fi
 #git init
 git add *
 git commit -m "termux"
 #git push -f origin main
+#echo https://"$1:$2@"github.com/sempogi/termuxurl.git
 git push -f https://"$1:$2@"github.com/sempogi/termuxurl.git
 
 
